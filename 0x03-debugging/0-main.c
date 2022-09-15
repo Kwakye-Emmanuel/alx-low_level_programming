ef6606dfbef8f589eac55552b2198e1c4d575329
Kwakye-Emmanuel <emmanuelkwakye593@gmail.com>
2022-09-15 15:41:14 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints whether an integer is positive or Negative.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
