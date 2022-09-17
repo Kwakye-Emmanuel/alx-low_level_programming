b931732d21ef2733f0b6f7607dfbec6f11c42dfc
Kwakye-Emmanuel <emmanuelkwakye593@gmail.com>
2022-09-17 16:38:20 -0700

    Task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints all possible Combinations of single digit numbers.
+* Return: 0 Always
+*/
+int main(void)
+{
+int i = '0';
+
+while (i <= '9')
+{
+putchar(i);
+if(i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+++i;
+}
+putchar('\n');
+return (0);
