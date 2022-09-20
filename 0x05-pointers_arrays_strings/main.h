4a9ed545acf9d24ecde9683ce2efe785ae164bdb
Kwakye-Emmanuel <emmanuelkwakye593@gmail.com>
2022-09-20 05:20:16 -0700

    Task0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,9 +6,9 @@
 * int prototypes()
 */
 
-void reset_t0_98(int *n)
-void swap_int(int *a, int *b)
-int_strlen(char *s);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
